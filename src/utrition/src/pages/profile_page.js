b9cf5ec992a011130e1543c9d7d6837c4f74c086
Jeffrey Wang <jwjeff200@gmail.com>
2022-11-16 20:02:03 -0500

    Adding navbar/title to site

---
+++ src/utrition/src/pages/profile_page.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div
+      style={{
+        color: "#187d04",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <h1>Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
