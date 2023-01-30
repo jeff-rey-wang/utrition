f2857062a853c3c07f4b1549c16380a53aab039b
Jeffrey Wang <jwjeff200@gmail.com>
2023-01-30 11:54:05 -0500

    Allocating areas for upload page

--- src/utrition/src/pages/upload_page.js
+++ src/utrition/src/pages/upload_page.js
@@ -2,33 +2,33 @@ import React from "react";
 import ImageUpload from "../components/imageupload/imageupload";
 import ManualUpload from "../components/manualupload/manualupload";
 import VoiceUpload from "../components/voiceupload/voiceupload";
+import "./upload_page.css";
 
 const Upload = () => {
   return (
     <div
       style={{
-        border: "solid",
-        borderColor: "#000",
         color: "#187d04",
         display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "90vh",
+        height: "100vh",
       }}
     >
-      <div style={{ position: "absolute", top: "30%", padding: "35px 0px" }}>
-        <h1>Upload an Image</h1>
-        <ImageUpload />
-      </div>
-      <div style={{ position: "absolute", top: "60%", padding: "35px 0px" }}>
-        <h1>Speak</h1>
-        <VoiceUpload />
-      </div>
-      <div style={{ position: "absolute", top: "110%", padding: "35px 0px" }}>
-        <h1>Please tell us what you ate!</h1>
+      <div class="left"></div>
+      <div class="right">
+        <div style={{ position: "absolute", top: "30%", padding: "35px 0px" }}>
+          <h1>Upload an Image</h1>
+          <ImageUpload />
+        </div>
+        <div style={{ position: "absolute", top: "60%", padding: "35px 0px" }}>
+          <h1>Speak</h1>
+          <VoiceUpload />
+        </div>
+        <div style={{ position: "absolute", top: "110%", padding: "35px 0px" }}>
+          <h1>Please tell us what you ate!</h1>
 
-        <h3>Example: I had three oranges and a grilled cheese sandwich</h3>
-        <ManualUpload />
+          <h3>Example: I had three oranges and a grilled cheese sandwich</h3>
+          <ManualUpload />
+        </div>
       </div>
     </div>
   );
