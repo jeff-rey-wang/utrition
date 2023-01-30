bf265c502740ff26daad364996a1ee86bef4eada
Jeffrey Wang <jwjeff200@gmail.com>
2023-01-30 11:25:48 -0500

    Added basic text input form

--- src/utrition/src/components/manualupload/manualupload.js
+++ src/utrition/src/components/manualupload/manualupload.js
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
 
 const ManualUpload = () => {
+  const [text, setText] = useState("");
+  const [responseData, setResponseData] = useState("");
+
+  function handleChange(event) {
+    this.setText({ value: event.target.value });
+  }
+
+  function handleSubmit(event) {}
+
   return (
     <div>
-      <div></div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Food:
+          <textarea value={text} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
--- src/utrition/src/pages/upload_page.js
+++ src/utrition/src/pages/upload_page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ImageUpload from "../components/imageupload/imageupload";
+import ManualUpload from "../components/manualupload/manualupload";
 import VoiceUpload from "../components/voiceupload/voiceupload";
 
 const Upload = () => {
@@ -15,11 +16,19 @@ const Upload = () => {
         height: "90vh",
       }}
     >
-      <h1 style={{ position: "absolute", top: "30%" }}>Upload an Image</h1>
-      <ImageUpload />
-      <div style={{ position: "absolute", top: "70%" }}>
+      <div style={{ position: "absolute", top: "30%", padding: "35px 0px" }}>
+        <h1>Upload an Image</h1>
+        <ImageUpload />
+      </div>
+      <div style={{ position: "absolute", top: "60%", padding: "35px 0px" }}>
         <h1>Speak</h1>
         <VoiceUpload />
+      </div>
+      <div style={{ position: "absolute", top: "110%", padding: "35px 0px" }}>
+        <h1>Please tell us what you ate!</h1>
+
+        <h3>Example: I had three oranges and a grilled cheese sandwich</h3>
+        <ManualUpload />
       </div>
     </div>
   );
