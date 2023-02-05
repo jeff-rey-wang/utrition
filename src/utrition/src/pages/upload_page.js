b6e10880e919407481f51f2bd7a60da7051d4cee
Jeffrey Wang <jwjeff200@gmail.com>
2023-02-04 19:38:23 -0500

    Fitting background to upload page

--- src/utrition/src/pages/upload_page.js
+++ src/utrition/src/pages/upload_page.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import ImageUpload from "../components/imageupload/imageupload";
-import TextUpload from "../components/textupload/textupload";
 import UploadContainer from "../components/uploadcontainer/uploadcontainer";
-import VoiceUpload from "../components/voiceupload/voiceupload";
 import "./upload_page.css";
 
 const Upload = () => {
@@ -21,27 +18,15 @@ const Upload = () => {
   return (
     <div className="upload-container">
       <div className="left">
-      <div style={{
-          justifyContent: "right",
-          marginTop:"40px",
-          display: "grid",
-          float: "right",
-          // left:"100px",
-          backgroundColor: "#EAFAF1",
-          width: "30%"
-          }}><img style={{ width: 400, height: 475 }}src={require("./upload.jpeg")} alt="Home Food"></img>
+        <div className="image-container">
+          <img
+            style={{ width: 400, height: 475 }}
+            src={require("./upload.jpeg")}
+            alt="Home Food"
+          ></img>
         </div>
       </div>
       <div className="right">
-      <div style={{
-          justifyContent: "right",
-          marginTop:"40px",
-          // display: "grid",
-          float: "right",
-          // left:"100px",
-          width: "65%"
-          }}>
-        </div>
         <div className="instructions container">
           <h2>How it works</h2>
           <h1>Upload Your Meal</h1>
@@ -68,15 +53,9 @@ const Upload = () => {
           </button>
         </div>
         <div className="components container">
-        <div style={{
-          marginTop:"15px",
-          height: "10px"
-          // display: "grid",
-          // left:"100px",
-          }}>
-        </div>
           <UploadContainer displayedUpload={currentUpload} />
         </div>
+        <div className="display container"></div>
       </div>
     </div>
   );
