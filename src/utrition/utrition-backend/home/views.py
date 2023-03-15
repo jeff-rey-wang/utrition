c6f604db2c8e161b214317109f18fc9124f916bb
Justina Srebrnjak <j.srebrnjak@hotmail.com>
2023-03-15 15:56:54 -0400

    Added functionality to edit settings page

--- src/utrition/utrition-backend/home/views.py
+++ src/utrition/utrition-backend/home/views.py
@@ -58,16 +58,16 @@ def display_profile():
     }
 
     # generate graph
-    profile_data = total_calories_per_day_summary_list()
-    dates = [d["date"] for d in profile_data]
-    calories = [c["sumPerDay"] for c in profile_data]
+    # profile_data = total_calories_per_day_summary_list()
+    # dates = [d["date"] for d in profile_data]
+    # calories = [c["sumPerDay"] for c in profile_data]
 
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dates, y=calories, mode="lines"))
-    fig.update_layout(
-        title="Calories per day", xaxis_title="Date", yaxis_title="Calories"
-    )
-    fig.write_image("data_graph.png")
+    # fig = go.Figure()
+    # fig.add_trace(go.Scatter(x=dates, y=calories, mode="lines"))
+    # fig.update_layout(
+    #     title="Calories per day", xaxis_title="Date", yaxis_title="Calories"
+    # )
+    # fig.write_image("data_graph.png")
 
     json_formatted_str = json.dumps(fullJSON, indent=2)
     return json_formatted_str
